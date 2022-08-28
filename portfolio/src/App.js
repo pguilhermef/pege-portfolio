8dd8df15d717a3ec29fdebe498767cd25023f6eb
pguilhermef <pguilhermesantos@hotmail.com>
2022-08-28 20:54:42 -0300

    [Feature] Adiciona opção de rotas ao clicar em alguma aba de navegação

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,27 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import About from './components/About';
+import Home from './components/Home';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Wallpapers from './components/Wallpapers';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <About />
+        <Switch>
+          <Route
+          exact
+          path='/wallpapers'
+          component={Wallpapers}
+          />
+
+          <Route
+          exact
+          path='/'
+          component={Home}
+          />
+        </Switch>
       <Footer />
     </div>
   );
