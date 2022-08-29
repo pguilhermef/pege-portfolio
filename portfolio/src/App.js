c04883938c04af1cd5e8f93d026c20071d5ef52a
pguilhermef <pguilhermesantos@hotmail.com>
2022-08-29 13:27:46 -0300

    [Refatoração] Readiciona caminho de css

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
+import './css/App.css';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
