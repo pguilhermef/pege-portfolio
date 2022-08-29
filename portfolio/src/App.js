96aec63cd1cb7727893de9508b6f7d2d9a50d92c
pguilhermef <pguilhermesantos@hotmail.com>
2022-08-28 22:23:55 -0300

    [Feature] Adiciona rota de Projetos

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Wallpapers from './components/Wallpapers';
 import NotFound from './components/NotFound';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -13,9 +14,16 @@ function App() {
         <Switch>
           <Route
           exact
+          path='/projects'
+          component={Projects}
+          />
+
+          <Route
+          exact
           path='/wallpapers'
           component={Wallpapers}
           />
+
 
           <Route
           exact
